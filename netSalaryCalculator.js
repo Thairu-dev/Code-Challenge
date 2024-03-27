ca8625f14cb3d92f0a373a883177f29f38b2b786
Joseph_Thairu <joejoement@gmail.com>
2024-03-27 13:11:33 +0300

    code errors fixed

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -1,30 +1,30 @@
-let grossSalary = prompt("Please enter your gross salary.")
-// setting tax rate for each income range
-let payee;
-    if (grossSalary <= 24000) {
-      payee = 0.1 * grossSalary;
-    } else if (grossSalary > 24000 && grossSalary <= 32333) {
-      payee = (24000*0.1) + 0.25 * (grossSalary - 24000);}
-      else if(grossSalary >=32334 && grossSalary <= 500000){
-        payee = (24000*0.1) + 0.25 *(grossSalary - 32333)+ 0.30 *(grossSalary - 3233)
-      }
+// let grossSalary = prompt("Please enter your gross salary.")
+// // setting tax rate for each income range
+// let payee;
+//     if (grossSalary <= 24000) {
+//       payee = 0.1 * grossSalary;
+//     } else if (grossSalary > 24000 && grossSalary <= 32333) {
+//       payee = (24000*0.1) + 0.25 * (grossSalary - 24000);}
+//       else if(grossSalary >=32334 && grossSalary <= 500000){
+//         payee = (24000*0.1) + 0.25 *(grossSalary - 32333)+ 0.30 *(grossSalary - 3233)
+//       }
 
-// setting nhif deductions for each income range
-let nhifDeductions;
-if (grossSalary <= 5999){nhifDeductions = 150}
-else if(grossSalary >= 6000 && grossSalary <= 7999){nhifDeductions = 300}
-else if (grossSalry >= 8000 && grossSalary <= 11999){nhifDeductions = 400}
-else if (grossSalry >= 12000 && grossSalary <= 14999){nhifDeductions = 500}
-else if (grossSalry >= 15000 && grossSalary <= 19999){nhifDeductions = 600}
-else if (grossSalry >= 20000 && grossSalary <= 24999){nhifDeductions = 750}
-else if (grossSalry >= 25000 && grossSalary <= 29999){nhifDeductions = 850}
-else if (grossSalry >= 30000 && grossSalary <= 34999){nhifDeductions = 900}
-else if (grossSalry >= 35000 && grossSalary <= 39999){nhifDeductions = 950}
-else if (grossSalry >= 40000 && grossSalary <= 44999){nhifDeductions = 1000}
-else if (grossSalry >= 45000 && grossSalary <= 49999){nhifDeductions = 1100}
-else if (grossSalry >= 50000 && grossSalary <= 59999){nhifDeductions = 1200}
-else if (grossSalry >= 60000 && grossSalary <= 69999){nhifDeductions = 1300}
-else if (grossSalry >= 70000 && grossSalary <= 79999){nhifDeductions = 1400}
-else if (grossSalry >= 80000 && grossSalary <= 89999){nhifDeductions = 1500}
-else if (grossSalry >= 90000 && grossSalary <= 99999){nhifDeductions = 1600}
-else{nhifDeductions = 1700}
+// // setting nhif deductions for each income range
+// let nhifDeductions;
+// if (grossSalary <= 5999){nhifDeductions = 150}
+// else if(grossSalary >= 6000 && grossSalary <= 7999){nhifDeductions = 300}
+// else if (grossSalry >= 8000 && grossSalary <= 11999){nhifDeductions = 400}
+// else if (grossSalry >= 12000 && grossSalary <= 14999){nhifDeductions = 500}
+// else if (grossSalry >= 15000 && grossSalary <= 19999){nhifDeductions = 600}
+// else if (grossSalry >= 20000 && grossSalary <= 24999){nhifDeductions = 750}
+// else if (grossSalry >= 25000 && grossSalary <= 29999){nhifDeductions = 850}
+// else if (grossSalry >= 30000 && grossSalary <= 34999){nhifDeductions = 900}
+// else if (grossSalry >= 35000 && grossSalary <= 39999){nhifDeductions = 950}
+// else if (grossSalry >= 40000 && grossSalary <= 44999){nhifDeductions = 1000}
+// else if (grossSalry >= 45000 && grossSalary <= 49999){nhifDeductions = 1100}
+// else if (grossSalry >= 50000 && grossSalary <= 59999){nhifDeductions = 1200}
+// else if (grossSalry >= 60000 && grossSalary <= 69999){nhifDeductions = 1300}
+// else if (grossSalry >= 70000 && grossSalary <= 79999){nhifDeductions = 1400}
+// else if (grossSalry >= 80000 && grossSalary <= 89999){nhifDeductions = 1500}
+// else if (grossSalry >= 90000 && grossSalary <= 99999){nhifDeductions = 1600}
+// else{nhifDeductions = 1700}
--- speedDetector.js
+++ speedDetector.js
@@ -5,7 +5,7 @@ function calculateDemerits(speed){
         let legalSpeed = 70;
         // declaring a condition that returns "ok" if the speed is below or equal to 70 which is the speed limit(legalSpeed) 
             if(speed <= legalSpeed){
-                return ("Ok")
+                return ("ok")
             }
             // declaring a variable that calculates the number of demrit points for drivers who are above the speed limit
         let demerits = (speed - legalSpeed)/5;
@@ -13,11 +13,12 @@ function calculateDemerits(speed){
         // declaring a condtion that returns the number of demerit points for drivers above the speed limit
         
         if(speed > legalSpeed && demerits <= 12){
-            return `Points: ${demerits}`;
+            return (`Points: ${demerits}`);
         }
         // declaring a condition that returns "licence suspended" if the drivers demerit points are greater than 12 points
         else {
-            return "License Suspended."
+            return (`License Suspended.`)
         }
+        
     }
     console.log(calculateDemerits())
